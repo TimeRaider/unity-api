9166256d213582e6c7f7a2fe3b7d08d76681806c
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-11-21 18:30:28 +0500

    test stub

--- test/callAPIMethod.spec.js
+++ test/callAPIMethod.spec.js
@@ -1,5 +1,7 @@
 import test from 'ava';
-import { getAPIPrefix } from '../src/callAPIMethod';
+import fetchMock from 'fetch-mock';
+
+import callAPIMethod, { getAPIPrefix } from '../src/callAPIMethod';
 
 test('getAPIPrefix', t => {
     t.is(getAPIPrefix('api'), '/api', 'relative path');
@@ -7,4 +9,14 @@ test('getAPIPrefix', t => {
     t.is(getAPIPrefix('http://api.example.com'), 'http://api.example.com', 'http url');
     t.is(getAPIPrefix('https://api.example.com'), 'https://api.example.com', 'http url');
     t.is(getAPIPrefix('//api.example.com'), '//api.example.com', 'protocol-less url');
+});
+
+test.before('mock fetch', () => {
+    fetchMock.get('/api/user/get/1', { name: 'user' });
+    fetchMock.post('/api/user/set/1', { name: 'user' });
+    fetchMock.delete('/api/user/delete/1?force=true', { name: 'user' });
+});
+
+test.after('unmock fetch', () => {
+    fetchMock.restore();
 });
--- test/createAPI.spec.js
+++ test/createAPI.spec.js
@@ -1,8 +1,19 @@
 import test from 'ava';
+import fetchMock from 'fetch-mock';
 import createAPI from '../src/createAPI';
 
 const api = createAPI();
 
 test('coverage', t => {
     t.pass()
+});
+
+test.before('mock fetch', () => {
+    fetchMock.get('/api/user/get/1', { name: 'user' });
+    fetchMock.post('/api/user/set/1', { name: 'user' });
+    fetchMock.delete('/api/user/delete/1?force=true', { name: 'user' });
+});
+
+test.after('unmock fetch', () => {
+    fetchMock.restore();
 });
